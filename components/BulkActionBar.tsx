514d1fa85956d19d15ff2445d88e79ef0c215bf1
vivek-quantbit <vivek.kumbhar@erpdata.in>
2026-01-08 11:47:33 +0530

    just Delete

--- components/BulkActionBar.tsx
+++ components/BulkActionBar.tsx
@@ -64,7 +64,7 @@ export function BulkActionBar({
         ) : (
           <>
             <Trash2 className="mr-2 h-3.5 w-3.5" />
-            Delete Selected
+            Delete
           </>
         )}
       </Button>
